6a8a01712ecd0a9eb0eae9c223b0b57c170000a1
Akash Rathore <90548368+SiriuSly04@users.noreply.github.com>
2022-10-10 09:35:37 +0530

    Update first.py

--- first.py
+++ first.py
@@ -17,3 +17,25 @@ data = web.DataReader(company, 'yahoo', start, end)
 
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 150
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train,(x_train.shape[0], x_train.shape[1], 1))
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences = True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
