0b53c3c59b45371c5354e233f21c682aa163b589
Akash Rathore <90548368+SiriuSly04@users.noreply.github.com>
2022-10-10 09:36:50 +0530

    Update first.py

--- first.py
+++ first.py
@@ -39,3 +39,20 @@ model.add(Dropout(0.2))
 model.add(LSTM(units=50))
 model.add(Dropout(0.2))
 model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss ='mean_squared_error')
+model.fit(x_train, y_train, epochs=1, batch_size=32)
+
+'''Test the model accuaracy on Existing data'''
+
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data["Close"]), axis=0)
+
+model_inputs = total_dataset[len(total_dataset)-len(test_data)-prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
